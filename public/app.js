1ec103e0fee7a438b82fed386e2f1d92bdb73a6a
Donald Highe <dhighe25@gmail.com>
2017-07-23 16:17:23 -0400

    Commiting to pull changes

---
+++ public/app.js
@@ -0,0 +1,9 @@
+(function(){
+const app = angular.module('theTest', []);
+
+app.controller('randomInformation', function(){
+  this.testing = 'This is a test'
+});
+
+
+})();
--- services/lol.js
+++ services/lol.js
@@ -1,7 +1,2 @@
 const fetch = require('node-fetch');
-const riotApi       = require("riot-api-nodejs");
-var classicApi      = new riotApi.ClassicApi(["RGAPI-f34c1742-86f7-43a4-bf48-d6fd0813fb28s"], riotApi.region_e.NA);
 
-var tournamentAPI   = new riotApi.TournamentAPI("RGAPI-f34c1742-86f7-43a4-bf48-d6fd0813fb28s");
-
-classicApi.getChampions('Ashe')
