b720c84f6571ccfea15e6d07217920a1b91d368b
Nicky Kwan <nickykwn@gmail.com>
2017-07-23 16:15:12 -0400

    success fetch of data

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const express       = require('express');
 const logger        = require('morgan');
 const path          = require('path');
 const bodyParser    = require('body-parser');
+const lolRoute      = require('./routes/lol.js');
 
 const app           = express();
 const port          = process.argv[2] || process.env.PORT || 3000;
 
-
+app.use('/freeChamps', lolRoute);
 // log requests
 app.use(logger('dev'));
 
 app.listen(port, console.log('Server is listening on port ', port));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
